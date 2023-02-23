ccff2d9d607dc276d4f54b5b21ea2da75d221429
Xiaodanguo2020 <guo.xiaodan.bk@gmail.com>
2023-02-23 14:07:13 +0100

    mission page basic setup

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,5 +1,23 @@
 import React from "react";
+import Button from "../components/Button";
+import NrInput from "../components/NrInput";
+import ContentContainer from "../components/ContentContainer";
+import "./Mission.css";
 
 export default function Mission() {
-  return <div>Mission</div>;
+  return (
+    <div>
+      <img src="./cute_monster6.png" alt="monster_pic"></img>
+      <ContentContainer>
+        <h2 className="title">You've got good sense of humor</h2>
+        <p className="enter-nr font-md">
+          Enter your phone number to get all sticker packs
+        </p>
+        <div className="input-container">
+          <NrInput />
+          <Button label="Funny" />
+        </div>
+      </ContentContainer>
+    </div>
+  );
 }
--- src/pages/PreLander.js
+++ src/pages/PreLander.js
@@ -2,18 +2,25 @@ import React from "react";
 import Button from "../components/Button";
 import ContentContainer from "../components/ContentContainer";
 import "./PreLander.css";
+// import { useHistory } from "react-router-dom";
 
 export default function PreLander() {
+  // const history = useHistory();
+
+  // const handleClick = (lable) => {
+  //   history.push(`/mission/:${lable}`);
+  // };
+
   return (
     <div>
-      <img src="./cute_monster6.png"></img>
+      <img src="./cute_monster6.png" alt="monster_pic"></img>
       <ContentContainer>
         <h2 className="title">
           Express your self with <br />
           <span className="bold">New</span>{" "}
           <span className="highlight">WhatsApp Stickers</span>
         </h2>
-        <p>choose your sticker type</p>
+        <p className="choose-text font-md">choose your sticker type</p>
         <div className="buttons-container">
           <Button label="Funny" />
           <Button label="Romance" />
