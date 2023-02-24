599a960a7097e2bd5636191980bfbc911f28e280
Xiaodanguo2020 <guo.xiaodan.bk@gmail.com>
2023-02-24 12:20:30 +0100

    svg icon added

--- src/components/NrInput.js
+++ src/components/NrInput.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./NrInput.css";
 import Button from "./Button";
+import mobile from "./mobile.svg";
 
 export default function NrInput({ phoneNr, setPhoneNr, onSubmit }) {
   return (
     <div>
       <form onSubmit={onSubmit} className="form-container">
-        <input
-          className="number-input"
-          type="tel"
-          name="phone"
-          value={phoneNr}
-          patter="[0-9]{10}"
-          onChange={(e) => {
-            setPhoneNr(e.target.value);
-          }}
-          required
-        />
+        <div className="input-container">
+          <img src={mobile} className="mobile-icon" alt="" />
+          <input
+            className="number-input"
+            type="tel"
+            name="phone"
+            value={phoneNr}
+            patter="[0-9]{10}"
+            onChange={(e) => {
+              setPhoneNr(e.target.value);
+            }}
+            required
+          />
+        </div>
         <Button type="submit" label="Submit to Subscribe" />
-        <i className="fas fa-mobile-screen-button"></i>
       </form>
     </div>
   );
